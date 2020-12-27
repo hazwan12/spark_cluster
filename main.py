0f22d690a269621096e33810ec5c07c74febef6e
hazwan12 <36067775+hazwan12@users.noreply.github.com>
2020-12-27 22:30:03 +0800

    Hello World Example

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ findspark.init()
 import pyspark
 
 spark = pyspark.sql.sparkSession.builder.getorcreate()
-df = spark.read.parquet("s3a://commoncrawl/cc-index/table/cc-main/warc")
-df = spark.read.json("s3a://commoncrawl/crawl-data/CC-MAIN-2020-50/wat.paths.gz")
+
+df = spark.sql("Select 'Hello World'")
+df.show()
+
+spark.stop()
--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 $SPARK_HOME/bin/pyspark \
     --master spark://spark-master:7077 \
     --deploy-mode client \
-    --name CommonCrawl \
-    --packages org.apache.hadoop:hadoop-aws:3.2.0
-	--conf spark.hadoop.fs.s3a.aws.credentials.provider=org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider
+    --name HelloWorld
 	
